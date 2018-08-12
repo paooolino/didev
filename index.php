47ae0760de040439d80ad970f994faea8c6aa0e1
paooolino <paooolino@gmail.com>
2018-08-12 08:25:47 +0200

    test locale

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-$result = setlocale(LC_ALL, "ita", "IT_it");
+$result = setlocale(LC_ALL, 0);
 var_dump($result);
-echo strftime("%B", time());
+echo "::." . strftime("%B", time());
 die();
 
   require("vendor/autoload.php");
