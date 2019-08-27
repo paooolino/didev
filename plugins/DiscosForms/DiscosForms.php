abc27920fa294571ce11a1f37f056e6616450244
Paolo <paooolino@gmail.com>
2019-08-27 09:28:26 +0200

    forms

--- plugins/DiscosForms/DiscosForms.php
+++ plugins/DiscosForms/DiscosForms.php
@@ -60,6 +60,65 @@ class DiscosForms {
     return $html;
   }
   
+  public function send_to_user($to, $name, $number="338.45.13.917") {
+    $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+    
+    $html = '
+Ciao ' . $name . ',<br><br>
+grazie per averci contattato.<br><br>
+Abbiamo ricevuto la tua richiesta. A breve verrai ricontattato dal nostro team.<br><br>
+Se la richiesta è urgente, contattaci telefonicamente o su WhatsApp al numero ' . $number . '<br><br>
+    ';
+    
+    $firma = '
+<div style="display: table; margin: 25px 0; text-size-adjust: none !important; -ms-text-size-adjust: none !important; -webkit-text-size-adjust: none !important;">
+	<div style="display: table-row;">
+		<div style="display: table-cell; width: 110px; border-right: 1px solid #ebebec;vertical-align: middle;">
+			<p style="font-family: Helvetica,Arial,sans-serif; font-size: 12px; line-height: 18px; margin-bottom: 10px;">
+				<a style="text-decoration: none;" href="http://www.discotecheitalia.it" title="Vai al sito">
+					<img src="http://www.albertoragnoli.it/img/db.png" height="54" width="82" alt="Discoteche Italia" border="0" style="margin-left: 7px">
+				</a>
+			</p>
+		</div>
+		<div style="display: table-cell;vertical-align: middle; padding: 0 0 0 20px">
+			<p style="font-family: Helvetica,Arial,sans-serif; font-size: 14px; line-height: 15px; text-transform: uppercase;  margin:0 0  10px 0; ">
+				DISCOTECHE ITALIA
+				<br /> <span style="color:#818181; font-size: 11px; text-transform: none; display: inline;letter-spacing: 0.3px">La miglior vetrina per locali ed eventi</span>
+			</p>
+			<p style="font-family: Helvetica,Arial,sans-serif; font-size: 12px; line-height: 17px;  margin:0 0  0px 0; ">
+				Via Padana Superiore 173/A, 25035 Ospitaletto - Brescia.<br />	
+				<strong>Email</strong> <a href="mailto:info@discotecheitalia.it" style="color: #ffaf00 !important; text-decoration: none !important;  display: inline;">info@discotecheitalia.it</a><br />
+				<strong>Website</strong> <a href="http://www.discotecheitalia.it" title="vai al sito" style="color: #ffaf00 !important; text-decoration: none !important;display: inline;">www.discotecheitalia.it</a>
+			</p>
+			
+		</div>
+	</div>
+</div>
+    ';
+    try {
+      $mail->isSMTP();                                      
+      $mail->setFrom('info@discotecheitalia.it', 'Discoteche Italia');
+      $mail->AddAddress($to);
+      
+      $mail->Username = $this->_SMTP_USER;
+      $mail->Password = $this->_SMTP_PASS;
+      $mail->Host = 'email-smtp.us-east-1.amazonaws.com';
+      
+      $mail->Subject = "[discotecheitalia] Abbiamo ricevuto la tua richiesta.";
+      $mail->Body = $html . $firma;
+      
+      $mail->SMTPAuth = true;     
+      $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+      $mail->Port = 587;                                    // TCP port to connect to
+      $mail->isHTML(true);     
+      
+      $mail->AltBody = strip_tags($html);
+      $mail->send();
+    } catch (Exception $e) {
+      //
+    }
+  }
+  
   public function send($title, $data) {
     $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
     try {
@@ -88,7 +147,7 @@ class DiscosForms {
       $mail->Host = 'email-smtp.us-east-1.amazonaws.com';
       
       // The subject line of the email
-      $site = isset($data["site_title"]) ? " " . $data["site_title"] : "";
+      $site = isset($data["site_title"]) ? " " . $data["site_title"] : "discotecheitalia";
       $mail->Subject = '[richiesta inviata dal sito' . $site . '] ' . $title;
 
       // The HTML-formatted body of the email
